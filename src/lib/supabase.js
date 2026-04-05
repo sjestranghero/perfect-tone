c12ad68fc7dee56cb93d44f5214b5d03ef90b7e6
Shaila Janine Santos <meejay049@gmail.com>
2026-04-05 11:25:53 +0800

    first commit

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://hjllatynavghogrmygmu.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhqbGxhdHluYXZnaG9ncm15Z211Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyODkwMzYsImV4cCI6MjA5MDg2NTAzNn0.3pjbVO-oJ4WUoVkoG75BKg4dnpl2v9_XbhJGqnaGhxg'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
